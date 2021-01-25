a2060332caede3e48a54d31471bae7dc818157c2
Mizraim Eliab <mizraimeliab168@gmail.com>
2021-01-25 12:17:56 -0600

    changes

--- src/app/components/account-registration/account-registration.component.ts
+++ src/app/components/account-registration/account-registration.component.ts
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./account-registration.component.css']
 })
 export class AccountRegistrationComponent implements OnInit {
-
+  regexp = new RegExp('[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}');
   constructor(public router: Router) { }
 
   ngOnInit(): void {
