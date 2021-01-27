a5b248cd20ef4a5dcc8ce25f6af5db356293edaf
Mizraim Eliab <mizraimeliab168@gmail.com>
2021-01-26 23:22:54 -0600

    captcha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { AccountRegistrationComponent } from './components/account-registration/account-registration.component';
+//import { AccountRegistrationComponent } from './components/account-registration/account-registration.component';
 // import { RouterModule, Routes } from '@angular/router';
+//import { RecaptchaModule } from "ng-recaptcha"; //captcha module
+import {
+  RECAPTCHA_SETTINGS,
+  RecaptchaModule,
+  RecaptchaSettings,
+} from 'ng-recaptcha';
 
-
+const globalSettings: RecaptchaSettings = { siteKey: '6Lfo6T0aAAAAAFSig1fD8Gbv5ljMqH7BKoaRDaGU' };
 
 @NgModule({
   declarations: [],
   imports: [
     BrowserModule,
-    AppRoutingModule // CLI adds AppRoutingModule to the AppModule's imports array
+    AppRoutingModule, // CLI adds AppRoutingModule to the AppModule's imports array
+    RecaptchaModule
     
   ],
-  providers: [],
+  providers: [{
+    provide: RECAPTCHA_SETTINGS,
+    useValue: globalSettings,
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/account-registration/account-registration.component.ts
+++ src/app/components/account-registration/account-registration.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { RecaptchaErrorParameters } from "ng-recaptcha";
+
 @Component({
   selector: 'app-account-registration',
   templateUrl: './account-registration.component.html',
   styleUrls: ['./account-registration.component.css']
 })
 export class AccountRegistrationComponent implements OnInit {
+  @ViewChild('recaptcha', {static: true }) recaptchaElement: ElementRef;
+
   regexp = new RegExp('[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}');
   constructor(public router: Router) { }
 
   ngOnInit(): void {
+    this.addRecaptchaScript();
   }
+
+  renderReCaptch() {
+    window['grecaptcha'].render(this.recaptchaElement.nativeElement, {
+      'sitekey' : '6Lfo6T0aAAAAAOrnUhfMqvhg67s0SB-2GELdcctK',
+      'callback': (response) => {
+          console.log(response);
+      }
+    });
+    
+  }
+
+  
+  addRecaptchaScript() {
+ 
+    window['grecaptchaCallback'] = () => {
+      this.renderReCaptch();
+    }
+ 
+    (function(d, s, id, obj){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) { obj.renderReCaptch(); return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "https://www.google.com/recaptcha/api.js?onload=grecaptchaCallback&amp;render=explicit";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'recaptcha-jssdk', this));
+ 
+  }
+ 
 
 }
