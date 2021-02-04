f4744206e923c5e8f7f188c91022e775ce6d1bc5
Mizraim Eliab <mizraimeliab168@gmail.com>
2021-02-04 01:32:19 -0600

    forgot_password_validations

--- src/app/components/recovery-password/recovery-password.component.ts
+++ src/app/components/recovery-password/recovery-password.component.ts
@@ -8,27 +8,32 @@ import { Router } from '@angular/router';
   providers: [RecoveryPasswordService]
 })
 export class RecoveryPasswordComponent implements OnInit {
-  show:Boolean
-  change: Boolean
-  email:String
-  code_verification:String
-  password:String
-  password_confirmation:String
-  RequestBody:any
-  
+  show:Boolean;
+  change: Boolean;
+  start:Boolean;
+  email:string;
+  code_verification:String;
+  password:string;
+  password_confirmation:string;
+  RequestBody:any;
+  regexpemail = new RegExp(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/);
+  regexppass = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/);
   constructor(public RecoveryService: RecoveryPasswordService, public router: Router) { }
 
   ngOnInit(): void {
     this.show = false
     this.change = false
+    this.start = false
   }
 
   SendCode(){
     this.RequestBody = {
       email: this.email
     }
-
-    this.RecoveryService.postRecoveryCode(JSON.parse(JSON.stringify(this.RequestBody)))
+    if (this.email == '' ||  this.regexpemail.test(this.email) == false){
+      alert("Please enter a valid email format and the field cannot be empty ");
+    }else{
+      this.RecoveryService.postRecoveryCode(JSON.parse(JSON.stringify(this.RequestBody)))
     .subscribe(res => {
       // Get response from the API
       console.log("Response: ");
@@ -40,7 +45,13 @@ export class RecoveryPasswordComponent implements OnInit {
       }else{
         this.show = false
       }
+      if(json.error_code == 'AccountDoesNotExist'){
+        alert("This account does not exist. Enter a valid email ");
+      }
+      
     });
+    }
+    
 
   }
 
@@ -58,8 +69,13 @@ export class RecoveryPasswordComponent implements OnInit {
       let json = JSON.parse(Response)
       if (json.status == 'success'){
         this.change = true
+        this.show = false
+        this.start = true
       }else{
         this.change = false
+      }
+      if (json.error_code == 'MailOrRecoveryCodeMatch' || json.error_code == 'ViolatedRules'){
+        alert("Security code does not match ");
       }
     });
   }
@@ -71,8 +87,10 @@ export class RecoveryPasswordComponent implements OnInit {
       password: this.password,
       password_confirmation: this.password_confirmation
     }
-
-    this.RecoveryService.postChangePassword(JSON.parse(JSON.stringify(this.RequestBody)))
+    if (this.regexppass.test(this.password) == false || this.regexppass.test(this.password_confirmation) == false){
+      alert("The format of the passwords is not the same or the passwords do not match and also cannot be empty ");
+    }else{
+      this.RecoveryService.postChangePassword(JSON.parse(JSON.stringify(this.RequestBody)))
     .subscribe(res => {
       // Get response from the API
       console.log("Response: ");
@@ -82,7 +100,11 @@ export class RecoveryPasswordComponent implements OnInit {
       if (json.status == 'success'){
         this.router.navigate(['/LogIn']);
       }
+ 
+      
     });
+    }
+    
   }
 
 }
